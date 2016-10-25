a0e1c05e5fadf16493a3d71f9743ea74390d02e9
squirlemaster42 <squirlemaster@gmail.com>
2016-10-25 18:42:02 -0400

    Started setting up code to test pid

--- 2016 Offseason Code/test/org/usfirst/frc/team1699/test/pid/PIDTest.java
+++ 2016 Offseason Code/test/org/usfirst/frc/team1699/test/pid/PIDTest.java
@@ -1,11 +1,15 @@
 package org.usfirst.frc.team1699.test.pid;
 
 import org.junit.Test;
+import org.usfirst.frc.team1699.robot.pid.PIDLoop;
+
+import edu.wpi.first.wpilibj.Encoder;
 
 public class PIDTest {
+	Encoder enc = new Encoder(null, null);
 	
 	@Test
 	public void pidTest(){
-		
+		PIDLoop pid = new PIDLoop("pid", .1, .1, .1, enc);
 	}
 }
