9cb4e75d2cdedb3505221843f7b13146cdbf51f6
Connor <thatging3rkid@users.noreply.github.com>
2017-01-07 15:08:31 -0500

    moved things around, updated to swerve-code

---
+++ src/org/usfirst/frc/team1699/robot/main/Constants.java
@@ -0,0 +1,37 @@
+/**
+ * Class that hold all of our constants and makes them easy to access
+ * 
+ * @author squirlemaster42, FIRST Team 1699
+ */
+package org.usfirst.frc.team1699.robot.main;
+
+public class Constants {
+
+	public static final int MOD1SPIN = 1;
+	public static final int MOD1DRIVE = 2;
+	public static final int MOD1SPINENCODER = 0;
+	public static final int MOD1DRIVEENCODER = 0;
+	
+	public static final int MOD2SPIN = 3;
+	public static final int MOD2DRIVE = 4;
+	public static final int MOD2SPINENCODER = 0;
+	public static final int MOD2DRIVEENCODER = 0;
+	
+	public static final int MOD3SPIN = 5;
+	public static final int MOD3DRIVE = 6;
+	public static final int MOD3SPINENCODER = 0;
+	public static final int MOD3DRIVEENCODER = 0;
+	
+	public static final int MOD4SPIN = 7;
+	public static final int MOD4DRIVE = 8;
+	public static final int MOD4SPINENCODER = 0;
+	public static final int MOD4DRIVEENCODER = 0;
+
+	public static final int XBOXDRIVEPORT = 0;
+
+	public static final int XBOXTRIGGERAXIS = 3;
+
+	public static final double X_DEADZONE = .07;
+	public static final double Y_DEADZONE = .07;
+	public static final double ROTATE_DEADZONE = .07;
+}
---
+++ src/org/usfirst/frc/team1699/utils/math/Math.java
@@ -0,0 +1,24 @@
+/**
+ * A class containing some math-based utilities.
+ * 
+ * @author squirlemaster42, FIRST Team 1699
+ */
+package org.usfirst.frc.team1699.utils.math;
+
+public class Math {
+
+	// May not be needed
+
+	public double joystickMagnitude(double x, double y) {
+		// Write method to take x and y produced by xbox joystick and transform
+		// it into a single value
+		// that is the magnitude of the joystick.
+		return java.lang.Math.sqrt(java.lang.Math.pow(x, 2) + java.lang.Math.pow(y, 2));
+	}
+
+	public double joystickDirection(double x, double y) {
+		// Write method to determine direction of joystick, should return value
+		// between 0 and 360.
+		return 0.0;
+	}
+}
